fa153eade96558617107362e9748c330a5b75f9f
vaikunth-e <vaikunth.elango@gmail.com>
2025-09-24 19:43:44 -0400

    sgd momentum optimizer

--- lab1/ece662/optim.py
+++ lab1/ece662/optim.py
@@ -69,7 +69,9 @@ def sgd_momentum(w, dw, config=None):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    v = config["momentum"] * v - config["learning_rate"] * dw
+    next_w = w + v
+
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
